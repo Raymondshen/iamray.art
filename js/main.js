150f22ce3ab6a5f64a37815e2b2667ff6d3946f8
Raymond Shen <raymond@netflixparty.com>
2022-05-31 17:31:51 -0700

    v1.0.3 Updated Favicon, Index Email, Animojis

--- js/main.js
+++ js/main.js
@@ -1,12 +1,72 @@
-let circleMouse = document.getElementById("mse-circle");
-let emailCopiedBanner = document.getElementById("nfn-banner");
-let emailBtn = document.getElementsByClassName("emailBtn");
+/* -- Document Ready -- */
+$(document).ready(function () {
+  /* Variables */
+  let circleMouse = document.getElementById("mse-circle");
+  let txtCopiedBanner = document.getElementById("nfn-banner");
+  let emailBtn = document.getElementsByClassName("emailBtn");
+  let idxEmailBtn = document.getElementById("idx-emailBtn-desktop");
 
-const followMouseMove = (e) => {
-  circleMouse.style.left = e.pageX + "px";
-  circleMouse.style.top = e.pageY + "px";
-};
+  /* Functions */
 
+  // Follow Mouse
+  let followMouseMove = (e) => {
+    circleMouse.style.left = e.pageX + "px";
+    circleMouse.style.top = e.pageY + "px";
+  };
+
+  // Show Banner
+  let showBanner = function (eName) {
+    eName.style.top = "1rem";
+    eName.style.opacity = "1";
+    setTimeout(() => {
+      eName.style.top = "-5rem";
+      eName.style.opacity = "0";
+    }, 3000);
+  };
+
+  // Animate Text Up & Down
+  let animateTxtIn = function () {
+    let emailName = document.getElementById("idx-contact-email-name");
+    let emailCopy = document.getElementById("idx-contact-email-copy");
+
+    emailCopy.style.transform = "translateY(-7.5vw)";
+    emailName.style.transform = "translateY(-7.5vw)";
+    emailCopy.style.opacity = "1";
+    emailName.style.opacity = "0";
+  };
+  let animateTxtOut = function () {
+    let emailName = document.getElementById("idx-contact-email-name");
+    let emailCopy = document.getElementById("idx-contact-email-copy");
+
+    emailCopy.style.transform = "translateY(0rem)";
+    emailName.style.transform = "translateY(0rem)";
+    emailCopy.style.opacity = "0";
+    emailName.style.opacity = "1";
+  };
+  /* Functions */
+
+  /* Loop */
+  // If Clicked on this Email Btn
+  for (var i = 0; i < emailBtn.length; i++) {
+    emailBtn[i].addEventListener("click", () => {
+      showBanner(txtCopiedBanner);
+    });
+  }
+  /* Loop */
+
+  /* Call Function */
+  document.addEventListener("mousemove", followMouseMove);
+  idxEmailBtn.addEventListener("mouseover", () => {
+    animateTxtIn();
+  });
+  idxEmailBtn.addEventListener("mouseout", () => {
+    animateTxtOut();
+  });
+  /* Call Function */
+});
+/* -- Document Ready -- */
+
+// Clipboard Copy
 let copyToClipboard = (element) => {
   var $tempInput = $("<input>");
   $("body").append($tempInput);
@@ -14,20 +74,3 @@ let copyToClipboard = (element) => {
   document.execCommand("copy");
   $tempInput.remove();
 };
-
-document.addEventListener("mousemove", followMouseMove);
-// Loop through the Class
-let showBanner = function (eName) {
-  eName.style.top = "1rem";
-  eName.style.opacity = "1";
-  setTimeout(() => {
-    eName.style.top = "-5rem";
-    eName.style.opacity = "0";
-  }, 3000);
-};
-
-for (var i = 0; i < emailBtn.length; i++) {
-  emailBtn[i].addEventListener("click", () => {
-    showBanner(emailCopiedBanner);
-  });
-}
