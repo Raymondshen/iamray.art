1fc856bf376dab6cd8f88cc5645d03bc1ae35b0b
RaymondShen <shenraymonds@gmail.com>
2024-02-04 11:25:50 -0600

    Added function to detect mobile 1.0.9

--- beta/js/main.js
+++ beta/js/main.js
@@ -1,24 +1,30 @@
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 $(function () {
-  $(".img-hover").hover(
-    function () {
-      $(this)
-        .find(".img-hidden")
-        .css({ transform: "rotate(10deg)", opacity: "1" });
-      $(this).find(".work-title").css({
-        textShadow: "-6px 6px var(--gold)",
-        transform: "translate(2px,-2px)",
-      });
-    },
-    function () {
-      $(this)
-        .find(".img-hidden")
-        .css({ transform: "rotate(0deg)", opacity: "0" });
-      $(this).find(".work-title").css({
-        textShadow: "0px 0px var(--gold)",
-        transform: "translate(0px,0px)",
-      });
-    }
-  );
+  if (!isMobile()) {
+      $(".img-hover").hover(
+          function () {
+              $(this)
+                  .find(".img-hidden")
+                  .css({ transform: "rotate(10deg)", opacity: "1" });
+              $(this).find(".work-title").css({
+                  textShadow: "-6px 6px var(--gold)",
+                  transform: "translate(2px,-2px)",
+              });
+          },
+          function () {
+              $(this)
+                  .find(".img-hidden")
+                  .css({ transform: "rotate(0deg)", opacity: "0" });
+              $(this).find(".work-title").css({
+                  textShadow: "0px 0px var(--gold)",
+                  transform: "translate(0px,0px)",
+              });
+          }
+      );
+  }
 });
 
 /* Smooth Scroll by Chris Coyler */
