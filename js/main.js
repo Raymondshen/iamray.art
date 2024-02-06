a613c1feb2f92e0c8d28f19e7ace2d1b4578b50e
RaymondShen <shenraymonds@gmail.com>
2024-02-06 17:56:37 -0600

    Updated New Site 2.6.2024

--- js/main.js
+++ js/main.js
@@ -1,98 +1,95 @@
-/* -- Document Ready -- */
 $(document).ready(function () {
-  /* Variables */
-  let circleMouse = document.getElementById("mse-circle");
-  let txtCopiedBanner = document.getElementById("nfn-banner");
-  let emailBtn = document.getElementsByClassName("emailBtn");
-  let idxEmailBtn = document.getElementById("idx-emailBtn-desktop");
-
-  /* Functions */
-
-  // Follow Mouse
-  let followMouseMove = (e) => {
-    circleMouse.style.left = e.pageX + "px";
-    circleMouse.style.top = e.pageY + "px";
-  };
-
-  // Show Banner
-  let showBanner = function (eName) {
-    eName.style.top = "1rem";
-    eName.style.opacity = "1";
-    setTimeout(() => {
-      eName.style.top = "-5rem";
-      eName.style.opacity = "0";
-    }, 1500);
-  };
-
-  // Animate Text Up & Down
-  let animateTxtIn = function () {
-    let emailName = document.getElementById("idx-contact-email-name");
-    let emailCopy = document.getElementById("idx-contact-email-copy");
-
-    emailCopy.style.transform = "translateY(-7.5vw)";
-    emailName.style.transform = "translateY(-7.5vw)";
-    emailCopy.style.opacity = "1";
-    emailName.style.opacity = "0";
-  };
-  let animateTxtOut = function () {
-    let emailName = document.getElementById("idx-contact-email-name");
-    let emailCopy = document.getElementById("idx-contact-email-copy");
-
-    emailCopy.style.transform = "translateY(0rem)";
-    emailName.style.transform = "translateY(0rem)";
-    emailCopy.style.opacity = "0";
-    emailName.style.opacity = "1";
-  };
-  /* Functions */
-
-  /* Loop */
-  // If Clicked on this Email Btn
-  for (var i = 0; i < emailBtn.length; i++) {
-    emailBtn[i].addEventListener("click", () => {
-      showBanner(txtCopiedBanner);
-    });
+  /* Detect if isMobile */
+  function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
   }
-  /* Loop */
+  /* Detect if isMobile */
 
-  /* Call Function */
-  document.addEventListener("mousemove", followMouseMove);
-  idxEmailBtn.addEventListener("mouseover", () => {
-    animateTxtIn();
+  /* On hover show work img */
+  $(function () {
+    if (!isMobile()) {
+      $(".img-hover").hover(
+        function () {
+          $(this)
+            .find(".img-hidden")
+            .css({ transform: "rotate(10deg)", opacity: "1" });
+          $(this).find(".work-title").css({
+            textShadow: "-6px 6px var(--gold)",
+            transform: "translate(2px,-2px)",
+          });
+        },
+        function () {
+          $(this)
+            .find(".img-hidden")
+            .css({ transform: "rotate(0deg)", opacity: "0" });
+          $(this).find(".work-title").css({
+            textShadow: "0px 0px var(--gold)",
+            transform: "translate(0px,0px)",
+          });
+        }
+      );
+    }
   });
-  idxEmailBtn.addEventListener("mouseout", () => {
-    animateTxtOut();
-  });
-  /* Call Function */
+  /* On Hover Show Work Img */
 
-  /* Index Project JS */
-  $(".idx-project-item")
-    .mouseenter(function () {
-      $(this)
-        .find(".idx-project-item-image")
-        .css({ transform: "rotate(8deg)", opacity: "1" });
-      $(this).find(".idx-project-item-name p").css({
-        textShadow: "-6px 6px var(--base-gold)",
-        transform: "translate(6px,-6px)",
-      });
-    })
-    .mouseleave(function () {
-      $(this)
-        .find(".idx-project-item-image")
-        .css({ transform: "rotate(0deg)", opacity: "0" });
-      $(this).find(".idx-project-item-name p").css({
-        textShadow: "0px 0px var(--base-gold)",
-        transform: "translate(0px,0px)",
-      });
-    });
-  /* Index Project JS */
+  /* Show/Hide Snack Bar */
+  $("#copyemail").click(function () {
+    $("#snackbar-wrapper").addClass('show');
+
+    setTimeout(function () {
+      $("#snackbar-wrapper").removeClass('show');
+    }, 1500);
+  });
+  /* Show/Hide Snack Bar */
+
+  /* Clip to board */
+  $('#copyemail').click(function() {
+    const contentToCopy = $('#copyemail').text();
 });
-/* -- Document Ready -- */
+});
 
-// Clipboard Copy
-let copyToClipboard = (element) => {
-  var $tempInput = $("<input>");
-  $("body").append($tempInput);
-  $tempInput.val($(element).text()).select();
-  document.execCommand("copy");
-  $tempInput.remove();
-};
+/* Smooth Scroll by Chris Coyler */
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
--- page/csp/old-site/js/work.js
+++
@@ -1,6 +0,0 @@
-// work.html functions
-$(".work-file-link").hover(function () {
-    $(this).find(".work-file-img").addClass('filter-invert');
-}, function () {
-    $(this).find(".work-file-img").removeClass('filter-invert');
-});
