0df27e80a8f05e0f46ef3792b7e7ba7014dcb661
RaymondShen <shenraymonds@gmail.com>
2025-05-14 22:37:33 -0500

    Updated JS

--- js/main.js
+++ js/main.js
@@ -10,6 +10,27 @@ $(document).ready(function () {
   /* On hover show work img */
   $(function () {
     if (!isMobile()) {
+      $(".img-hover").hover(
+        function () {
+          $(this)
+            .find(".img-hidden")
+            .css({ transform: "rotate(10deg)", opacity: "1" });
+          $(this).find(".work-title").css({
+            textShadow: "-6px 6px var(--gold)",
+            transform: "translate(2px,-2px)",
+          });
+        },
+        function () {
+          $(this)
+            .find(".img-hidden")
+            .css({ transform: "rotate(0deg)", opacity: "0" });
+          $(this).find(".work-title").css({
+            textShadow: "0px 0px var(--gold)",
+            transform: "translate(0px,0px)",
+          });
+        }
+      );
+
       $(".work-row").each(function () {
         const $row = $(this);
         const $imgContainer = $row.find(".work-img");
